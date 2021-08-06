1e1478be18e92838ec37a77925ee3b3a149c710e
berkingurcan <70534820+berkingurcan@users.noreply.github.com>
2021-08-06 12:59:53 +0300

    Add files via upload

---
+++ assignment4.R
@@ -0,0 +1,31 @@
+plot_car_data <- function(filename){
+  # your code here
+}
+
+predict_mpg <- function(p,a,w){
+  # your code here
+}
+
+first_model <- function(filename){
+  # your code here
+}
+
+second_model <- function(filename){
+  # your code here
+}
+
+third_model <- function(filename){
+  # your code here
+}
+
+plotmodels <- function(filename){
+  # your code here
+}
+
+fenwick_workforce <- function(years, initial, rates){
+  # your code here
+}
+
+plot_workforce_time <- function(years, initial, rates){
+  # your code here
+}
